0344cbdca2afacb3aeeed002bb842518906b18c3
Badboy <constantinradulicea@gmail.com>
2024-01-01 17:05:56 +0200

    working on connection with pixy

--- main.cpp
+++ main.cpp
@@ -18,40 +18,104 @@ using namespace std;
 // https://notisrac.github.io/FileToCArray/
 
 
+HANDLE serialSetup(const char *port = "\\\\.\\COM1", int baudrate = 115200) {
+	// Open serial port
+	HANDLE serialHandle;
+
+	serialHandle = CreateFileA(port, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+	// Do some basic settings
+	DCB serialParams = { 0 };
+	serialParams.DCBlength = sizeof(serialParams);
+
+	GetCommState(serialHandle, &serialParams);
+	serialParams.BaudRate = baudrate;
+	serialParams.ByteSize = 8;
+	serialParams.StopBits = TWOSTOPBITS;
+	serialParams.Parity = NOPARITY;
+	SetCommState(serialHandle, &serialParams);
+
+	// Set timeouts
+	COMMTIMEOUTS timeout = { 0 };
+	timeout.ReadIntervalTimeout = 50;
+	timeout.ReadTotalTimeoutConstant = 50;
+	timeout.ReadTotalTimeoutMultiplier = 50;
+	timeout.WriteTotalTimeoutConstant = 50;
+	timeout.WriteTotalTimeoutMultiplier = 10;
+
+	SetCommTimeouts(serialHandle, &timeout);
+	return serialHandle;
+}
+
+static uint16_t serial_read_uint16_t(HANDLE serialHandle) {
+	uint16_t res;
+	DWORD bytesRead = 0;
+	DWORD bytesToRead = sizeof(res);
+
+	while (bytesToRead > 0)
+	{
+		BOOL awd = ReadFile(serialHandle, &(((char*)&res)[bytesRead]), bytesToRead, &bytesRead, NULL);
+		bytesToRead -= bytesRead;
+	}
+	return res;
+}
+
+
+static uint8_t serial_read_uint8_t(HANDLE serialHandle) {
+	uint8_t res;
+	DWORD bytesRead = 0;
+	DWORD bytesToRead = sizeof(res);
+
+	while (bytesToRead > 0)
+	{
+		BOOL awd = ReadFile(serialHandle, &(((char*)&res)[bytesRead]), bytesToRead, &bytesRead, NULL);
+		bytesToRead -= bytesRead;
+	}
+	return res;
+}
+
+static void serial_write_uint16_t(HANDLE serialHandle, uint16_t val) {
+	DWORD bytesWrite = 0;
+	DWORD bytesToWrite = sizeof(val);
+
+	while (bytesToWrite > 0)
+	{
+		BOOL awd = WriteFile(serialHandle, &(((char*)&val)[bytesWrite]), bytesToWrite, &bytesWrite, NULL);
+		bytesToWrite -= bytesWrite;
+	}
+}
+
+
+
 
 
 int main() {
 	clock_t start, stop, delta;
+	HANDLE serialHandle;
 
 	BitMatrix result(UNTITLED_HEIGHT, UNTITLED_WIDTH);
-	// Open serial port
-	//HANDLE serialHandle;
-	//char buffer[] = "ciao\n";
 
-	//serialHandle = CreateFileA("\\\\.\\COM1", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+	//serialHandle = serialSetup("\\\\.\\COM6", 115200);
 
-	//// Do some basic settings
-	//DCB serialParams = { 0 };
-	//serialParams.DCBlength = sizeof(serialParams);
+	//int r, g, b;
+	//uint16_t x, y;
 
-	//GetCommState(serialHandle, &serialParams);
-	//serialParams.BaudRate = 9600;
-	//serialParams.ByteSize = 8;
-	//serialParams.StopBits = TWOSTOPBITS;
-	//serialParams.Parity = NOPARITY;
-	//SetCommState(serialHandle, &serialParams);
 
-	//// Set timeouts
-	//COMMTIMEOUTS timeout = { 0 };
-	//timeout.ReadIntervalTimeout = 50;
-	//timeout.ReadTotalTimeoutConstant = 50;
-	//timeout.ReadTotalTimeoutMultiplier = 50;
-	//timeout.WriteTotalTimeoutConstant = 50;
-	//timeout.WriteTotalTimeoutMultiplier = 10;
+	//for (y = 0; y < 190; y++)
+	//{
+	//	for (x = 0; x < 300; x++)
+	//	{
+	//		serial_write_uint16_t(serialHandle, x);
+	//		serial_write_uint16_t(serialHandle, y);
+	//		r = serial_read_uint8_t(serialHandle);
+	//		g = serial_read_uint8_t(serialHandle);
+	//		b = serial_read_uint8_t(serialHandle);
+	//		std::cout << x << " " << y << " " << r << " " << g << " " << b << std::endl;
+	//		//std::cout << x << " " << y << std::endl;
+	//	}
+	//}
 
-	//SetCommTimeouts(serialHandle, &timeout);
-
-	//WriteFile(serialHandle, buffer, sizeof(buffer), NULL, NULL);
+	//CloseHandle(serialHandle);
 
 
 	writeMatlabImage("image.csv", IMG, UNTITLED_WIDTH, UNTITLED_HEIGHT);
--- rgb2hsv.h
+++ rgb2hsv.h
@@ -23,9 +23,9 @@ static HSVcolor rgb2hsv(RGBcolor rgbColor) {
 	HSVcolor hsvColor;
 	float del_R, del_G, del_B;
 
-	float var_R = ((float)rgbColor.R / 255.0);
-	float var_G = ((float)rgbColor.G / 255.0);
-	float var_B = ((float)rgbColor.B / 255.0);
+	float var_R = ((float)rgbColor.R / 255.0f);
+	float var_G = ((float)rgbColor.G / 255.0f);
+	float var_B = ((float)rgbColor.B / 255.0f);
 
 	float var_Min = MIN(MIN(var_R, var_G), var_B);    //Min. value of RGB
 	float var_Max = MAX(MAX(var_R, var_G), var_B);    //Max. value of RGB
@@ -33,25 +33,26 @@ static HSVcolor rgb2hsv(RGBcolor rgbColor) {
 
 	hsvColor.V = var_Max;
 
-	if (del_Max == 0)                     //This is a gray, no chroma...
+	if (del_Max == 0.0f)                     //This is a gray, no chroma...
 	{
-		hsvColor.H = 0;
-		hsvColor.S = 0;
+		hsvColor.H = 0.0f;
+		hsvColor.S = 0.0f;
 	}
 	else                                    //Chromatic data...
 	{
 		hsvColor.S = del_Max / var_Max;
 
-		del_R = (((var_Max - var_R) / 6.0) + (del_Max / 2)) / del_Max;
-		del_G = (((var_Max - var_G) / 6.0) + (del_Max / 2)) / del_Max;
-		del_B = (((var_Max - var_B) / 6.0) + (del_Max / 2)) / del_Max;
+		del_R = (((var_Max - var_R) / 6.0f) + (del_Max / 2.0f)) / del_Max;
+		del_G = (((var_Max - var_G) / 6.0f) + (del_Max / 2.0f)) / del_Max;
+		del_B = (((var_Max - var_B) / 6.0f) + (del_Max / 2.0f)) / del_Max;
 
 		if (var_R == var_Max) hsvColor.H = del_B - del_G;
-		else if (var_G == var_Max) hsvColor.H = (1.0 / 3.0) + del_R - del_B;
-		else if (var_B == var_Max) hsvColor.H = (2.0 / 3.0) + del_G - del_R;
+		else if (var_G == var_Max) hsvColor.H = (1.0f / 3.0f) + del_R - del_B;
+		else if (var_B == var_Max) hsvColor.H = (2.0f / 3.0f) + del_G - del_R;
 
-		if (hsvColor.H < 0.0) hsvColor.H += 1.0;
-		if (hsvColor.H > 1.0) hsvColor.H -= 1.0;
+		if (hsvColor.H < 0.0f) hsvColor.H += 1.0f;
+		if (hsvColor.H > 1.0f) hsvColor.H -= 1.0f;
 	}
+
 	return hsvColor;
 }
